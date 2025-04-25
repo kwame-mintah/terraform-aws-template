1ff3fc481300e6138a26fd7361dc84ee3b577ab5
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2025-04-25 23:01:25 +0100

    refactor(outputs): fix typo reference name
    should be available_zones

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # The Availability Zones data source allows access to the list of AWS Availability Zones 
 # which can be accessed by an AWS account within the region configured in the provider.
-data "aws_availability_zones" "avaliabile_zones" {}
+data "aws_availability_zones" "available_zones" {}
 
 # Data source to get the access to the effective Account ID, User ID, and ARN 
 # in which Terraform is authorized.
